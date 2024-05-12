906fab2bbadca27cb0b3ef8d3d83b107ca57b7d4
rachitsgh <rachit2351@gmail.com>
2024-05-12 10:58:50 +0530

    added scroller functionality

--- frontend/src/components/MovieList.js
+++ frontend/src/components/MovieList.js
@@ -6,18 +6,19 @@ function MovieList({title,movies}) {
     // const movieTitle=movies[4].title;
     // const poster_path=movies[4].poster_path;
   return (
-    <div>
-        <div>
-            <h1>{title}</h1>
-            <div>
+    <div className='px-6'>
+      <h1 className='text-white text-3xl py-2'>{title}</h1>
+        <div className='flex overflow-x-scroll space-x-4 py-2'>
+            <div className='flex shrink-0'>
                 {/* {console.log(movies)} */}
-                <MovieCard movies={movies}/>
+                {movies.map((movie)=> <MovieCard key={movie?.id} Movie_title={movie?.title} poster_path={movie?.poster_path}/>)}
                 {/* poster_path={poster_path} */}
                 {/* /> */}
             </div>
         </div>
     </div>
   )
+
 }
 
 export default MovieList
