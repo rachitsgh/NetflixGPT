91911257ab03f37bd95a5604078c4a5f16c4ef28
rachitsgh <rachit2351@gmail.com>
2024-05-12 10:55:31 +0530

    added useTopRatedMovies hooks

---
+++ frontend/src/hooks/useTopRatedMovies.js
@@ -0,0 +1,22 @@
+import { useDispatch } from "react-redux";
+import { API_options } from "../utils/constant";
+import { addTopRatedMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+
+const useTopRatedMovies=()=>{
+    const dispatch=useDispatch();
+
+    const getTopRatedMovies=async()=>{
+        const data = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',API_options);
+        const json=await data.json()
+        // console.log("hello"+json.results);
+        dispatch(addTopRatedMovies(json.results))
+
+    }
+    useEffect(()=>
+    getTopRatedMovies,[])
+
+}
+
+export default useTopRatedMovies;
