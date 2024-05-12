9e4cde2ea692ff899d2095c11ecaefcabdec1f10
rachitsgh <rachit2351@gmail.com>
2024-05-12 10:55:59 +0530

    added usePopularMovies hooks

---
+++ frontend/src/hooks/usePopularMovies.js
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux"
+import { API_options } from "../utils/constant";
+import { useEffect } from "react";
+import { addPopularMovies } from "../utils/moviesSlice";
+const usePopularMovies =()=>{
+    const dispatch = useDispatch();
+    //fetch the data from API and update store
+  
+    const getPopularMovies=async()=>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',API_options);
+    const json=await data.json()
+    // console.log("hello"+json.results);
+    dispatch(addPopularMovies(json.results))
+    
+  } 
+  useEffect(()=>{
+    getPopularMovies();
+  },[])
+}
+
+export default usePopularMovies;
