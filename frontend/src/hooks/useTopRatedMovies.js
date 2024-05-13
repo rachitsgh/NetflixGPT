9d5c1167626a69b700d66e40c1b0f72686c8700f
rachitsgh <rachit2351@gmail.com>
2024-05-13 16:12:12 +0530

    did memoization

--- frontend/src/hooks/useTopRatedMovies.js
+++ frontend/src/hooks/useTopRatedMovies.js
@@ -2,10 +2,14 @@ import { useDispatch } from "react-redux";
 import { API_options } from "../utils/constant";
 import { addTopRatedMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 
 const useTopRatedMovies=()=>{
     const dispatch=useDispatch();
+
+    const topRatedMovies=useSelector(store=>store.movies.topRatedMovies)
+
 
     const getTopRatedMovies=async()=>{
         const data = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',API_options);
@@ -15,7 +19,7 @@ const useTopRatedMovies=()=>{
 
     }
     useEffect(()=>
-    getTopRatedMovies,[])
+    topRatedMovies.length==0 && getTopRatedMovies,[])
 
 }
 
