6b0033b80f8e7ae63a648d15e1da2b31225940fd
rachitsgh <rachit2351@gmail.com>
2024-05-13 16:19:01 +0530

    finishing touches

--- frontend/src/components/MovieCard.js
+++ frontend/src/components/MovieCard.js
@@ -4,6 +4,7 @@ import { IMG_CDN_URL } from '../utils/constant'
 function MovieCard({Movie_title,poster_path}) {
   // console.log(movies);
   // console.log(movies[0]?.title)
+  if(!poster_path) return null
   return (
     <div className='w-48 pr-4'>
       <img alt={Movie_title}
