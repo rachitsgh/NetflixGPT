acd33ea79302a3c5af573c818d282b3751209d33
rachitsgh <rachit2351@gmail.com>
2024-05-13 16:09:02 +0530

    added gpt and config Slice to it

--- frontend/src/utils/appStore.js
+++ frontend/src/utils/appStore.js
@@ -1,12 +1,15 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesReducer from "./moviesSlice";
-
+import gptReducer from "./gptSlice";
+import confifReducer from "./configSlice"; 
 const appStore = configureStore(
     {
         reducer:{
             user:userReducer,
-            movies:moviesReducer
+            movies:moviesReducer,
+            gpt:gptReducer,
+            config:confifReducer
         }
     }
 ); 
