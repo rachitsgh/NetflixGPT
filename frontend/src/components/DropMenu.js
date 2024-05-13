246594d0017f8d33f0b0704b07e5d2520394469d
rachitsgh <rachit2351@gmail.com>
2024-05-13 16:44:10 +0530

    made DropMenu Multilingual

--- frontend/src/components/DropMenu.js
+++ frontend/src/components/DropMenu.js
@@ -1,16 +1,33 @@
 import React from 'react'
+import { auth } from '../utils/firebase';
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import lang from "../utils/languageConstants"
+import { useSelector } from 'react-redux';
 
 function DropMenu(){
+  const langKey = useSelector(store=>store.config.lang)
+  const navigate=useNavigate();
+  const HandleSignout=()=>{
+    signOut(auth).then(() => {
+      navigate("/");
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+      navigate("/error")
+    });
+  } 
   return (
-    <div className='text-white w-[15%] p-12 bg-black bg-opacity-80 absolute my-36 mx-auto right-0 top-[1%] left-[71%] flex-row justify-center items-center'>
+    <div className='text-white w-[20%] -mt-  p-12 bg-black bg-opacity-80 absolute mx-auto right-0  left-[75%] flex-row justify-center items-center'>
          <ul>
+            <li><div className='bg-red w-full border-red-600 h-2'></div></li>
             <li>Children</li>
-            <li>Manage Profile</li>
-            <li>Transfer Profile</li>
-            <li>Account</li>
-            <li>Help center</li>
+            <li>{lang[langKey].manageProfile}</li>
+            <li>{lang[langKey].transferProfile}</li>
+            <li>{lang[langKey].account}</li>
+            <li>{lang[langKey].helpCenter}</li>
             <div className= 'bg-white'></div>
-            <li>signOut of Netflix</li>
+            <li onClick={HandleSignout}>{lang[langKey].signOutOfNetflix}</li>
         </ul>
     </div>
   )
