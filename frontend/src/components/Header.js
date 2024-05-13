70dd4fbf30c076ba49956806d835f9bdf880457b
rachitsgh <rachit2351@gmail.com>
2024-05-13 16:22:34 +0530

    added language selector

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,15 +10,23 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { UseDispatch } from 'react-redux';
 import { addUser,removeUser } from '../utils/userSlice';
 import { onLog } from 'firebase/app';
-import { LOGO_URL } from '../utils/constant';
+import { LOGO_URL, USER_AVATAR } from '../utils/constant';
+import { toggleGptSearch } from '../utils/gptSlice';
+import { SUPPORTED_LANGUAGES } from '../utils/constant';
+import { changeLanguage } from '../utils/configSlice';
+import lang from '../utils/languageConstants';
 
 
 const Header = () => {
   const [isVerify,setIsVerify]=useState(false);
   const navigate = useNavigate( );
   const dispatch = useDispatch();
+  const[isHover,setIsHover]=useState(false);
   // ConstantS
   const user = useSelector(store=>store.user);
+  const showGptSearch=useSelector(store=>store.gpt.showGptSearch);
+  const langKey=useSelector(store=>store.config.lang);
+  
 
   useEffect(()=>{
     
@@ -50,29 +58,51 @@ const Header = () => {
 
 
   const HandleHover=()=>{
+    setIsHover(!isHover);
+  }
 
+  const HandleGPTSearchClick=()=>{
+    //Toggle GPT Search
+    dispatch(toggleGptSearch());
   }
-  const HandleSignout=()=>{
-    signOut(auth).then(() => {
-      navigate("/");
-      // Sign-out successful.
-    }).catch((error) => {
-      // An error happened.
-      navigate("/error")
-    });
+  const handleLanguageChange =(e)=>{
+    // console.log(e.target.value)
+    dispatch(changeLanguage(e.target.value));
   }
+  
   return (
-    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
+    <div className='fixed w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
       
        <img className="w-44" src={LOGO_URL} alt='logo' />
+       {!user &&
+        <select onChange={handleLanguageChange} className=' flex justify-between h-12 mt-3 text-white w-24 rounded-md bg-gray-600 bg-opacity-50'>
+            {SUPPORTED_LANGUAGES.map(lang=>
+            <option key={lang.identifier} value={lang.identifier}>
+              {lang.name}
+              </option>)}
+          </select>
+        }
        
-       {user && <div className='flex p-2 gap-2'>
-         <div className='flex-row justify-start '>
-            <img className='w-20 h-12' src={user.photoURL} alt='userIcon'/>
-            {/* <DropMenu/> */}
+       {user && 
+       <div className='flex p-2 gap-2 align-middle'>
+           <select onChange={handleLanguageChange} className=' flex justify-between h-12 mt-3 text-white w-24 rounded-md bg-gray-600 bg-opacity-50'>
+            {SUPPORTED_LANGUAGES.map(lang=>
+            <option key={lang.identifier} value={lang.identifier}>
+              {lang.name}
+              </option>)}
+          </select>
+          
+          <button className= 'w-52 py-1 px-2 mx-4 my-2 w-full hover:bg-opacity-35 bg-gray-600 bg-opacity-50 text-white round-lg'
+          onClick={HandleGPTSearchClick}
+          >{showGptSearch?lang[langKey].explore+" <3":"GPT "+lang[langKey].search}</button>
+          <div className='flex flex-shrink-0 '> 
+            <div className='flex-row justify-start'onMouseLeave={HandleHover} onMouseEnter={HandleHover}  >
+              <img className='w-12 my-2  h-12 mt-4' src={USER_AVATAR} alt='userIcon'/>
+              {isHover&& <DropMenu/>}
+            </div>
+            {/* <button onClick={HandleSignout} className='p-4 py-2 my-6 w-full bg-red-700 rounded-md text-white '>Sign Out</button> */}
           </div>
-          <button onClick={HandleSignout} className='p-4 py-2 my-6 w-full bg-red-700 rounded-md text-white '>Sign Out</button>
-       </div>
+        </div>
       }
     </div>
   )
