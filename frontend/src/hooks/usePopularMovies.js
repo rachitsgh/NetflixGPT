09e750cccaae7dd7c82731498939fcab5d3c6e34
rachitsgh <rachit2351@gmail.com>
2024-05-13 16:12:45 +0530

    did memoization

--- frontend/src/hooks/usePopularMovies.js
+++ frontend/src/hooks/usePopularMovies.js
@@ -2,9 +2,12 @@ import { useDispatch } from "react-redux"
 import { API_options } from "../utils/constant";
 import { useEffect } from "react";
 import { addPopularMovies } from "../utils/moviesSlice";
+import { useSelector } from "react-redux";
 const usePopularMovies =()=>{
     const dispatch = useDispatch();
     //fetch the data from API and update store
+
+    const popularMovies=useSelector(store=>store.movies.popularMovies)
   
     const getPopularMovies=async()=>{
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',API_options);
@@ -14,7 +17,7 @@ const usePopularMovies =()=>{
     
   } 
   useEffect(()=>{
-    getPopularMovies();
+    popularMovies.length==0 &&getPopularMovies();
   },[])
 }
 
