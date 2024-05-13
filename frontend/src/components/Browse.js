e4780992d8cc8342ec7e011461351bace007f484
rachitsgh <rachit2351@gmail.com>
2024-05-13 16:48:25 +0530

    component composition of gptsearc in Browse

--- frontend/src/components/Browse.js
+++ frontend/src/components/Browse.js
@@ -3,12 +3,29 @@ import Header from './Header'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import MainContainer from './MainContainer';
 import SecondaryContainer from './SecondaryContainer';
+import usePopularMovies from '../hooks/usePopularMovies';
+import useTopRatedMovies from '../hooks/useTopRatedMovies';
+import useUpcomingMovies from '../hooks/useUpcomingMovies';
+import GPTSearch from './GPTSearch';
+import { useSelector } from 'react-redux';
 
 const Browse = () => {
+
   useNowPlayingMovies();  
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
+  const showGptSearch = useSelector(store=>store.gpt.showGptSearch);
+
   return (
     <div>
       <Header/>
+      {showGptSearch?<GPTSearch/>:
+      <>
+        <MainContainer/>
+        <SecondaryContainer/>
+      </>
+      }
       {/* 
         MainContainer
           - video background
@@ -17,8 +34,6 @@ const Browse = () => {
           - MovieList * N
             - cards * N
       */}
-      <MainContainer/>
-      <SecondaryContainer/>
     </div>
   )
 }
