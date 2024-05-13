e361f29ecbbd46f24b6d2e2ac70b22336deaf8e9
rachitsgh <rachit2351@gmail.com>
2024-05-13 16:00:58 +0530

    added openAi configuration for GPT search functionality

---
+++ frontend/src/utils/openai.js
@@ -0,0 +1,10 @@
+import OpenAI from 'openai';
+import { OPEN_AI_KEY } from './constant';
+
+
+const openai = new OpenAI({
+    apiKey:OPEN_AI_KEY,
+    dangerouslyAllowBrowser: true
+  });
+
+  export default openai;
